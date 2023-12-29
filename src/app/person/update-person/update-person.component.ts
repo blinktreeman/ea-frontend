2654f089350971d537346f19e54fd0096f6b06d0
Eugene Elantsev <blinktreeman@gmail.com>
2023-12-29 23:58:54 +0900

    initial commit

---
+++ src/app/person/update-person/update-person.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-person',
+  standalone: true,
+  imports: [],
+  templateUrl: './update-person.component.html',
+  styleUrl: './update-person.component.css'
+})
+export class UpdatePersonComponent {
+
+}
